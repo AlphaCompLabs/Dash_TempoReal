f96a1664f60ef90c9655e71a309a748d3488b64a
“elisanuness” <elisanunessf.04@gmail.com>
2025-09-11 14:48:04 -0300

    add: Sidebar component

--- FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true, 
-  imports: [],
+  imports: [SidebarComponent,
+    RouterOutlet
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
