9ea1e1232bdb109d69b4ac2d506fdddbcf2a8a6d
“elisanuness” <elisanunessf.04@gmail.com>
2025-09-12 10:55:17 -0300

    add: Footer component

--- FrontEnd/FrontEnd-Angular/src/app/app.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent ],
+    HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { FooterComponent } from '../../components/footer/footer.component'; 
 
 @Component({
   selector: 'app-home',
   standalone: true, 
   imports: [SidebarComponent,
-    RouterOutlet
+      FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
