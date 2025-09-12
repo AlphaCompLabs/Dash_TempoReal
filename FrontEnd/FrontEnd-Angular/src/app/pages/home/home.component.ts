37a224455446639b54c1c89e3e612c8db3b21c78
G2Martins <gustavogripaldi@outlook.com>
2025-09-11 23:48:19 -0300

    fix: icone Site e Warning ng Serve

--- FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true, 
-  imports: [SidebarComponent,
-    RouterOutlet
-  ],
+  imports: [SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
