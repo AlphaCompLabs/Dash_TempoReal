80a54b40b1bc37c6bd58bc0513526be68f8faccf
“elisanuness” <elisanunessf.04@gmail.com>
2025-09-12 18:44:24 -0300

    add: Main-chart component

--- FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { FooterComponent } from '../../components/footer/footer.component'; 
+import { MainChartComponent } from '../../components/main-chart/main-chart.component';
 
 @Component({
   selector: 'app-home',
   standalone: true, 
   imports: [SidebarComponent,
-      FooterComponent
+      FooterComponent,
+      MainChartComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
