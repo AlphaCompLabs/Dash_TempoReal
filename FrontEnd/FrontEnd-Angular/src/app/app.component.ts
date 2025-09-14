0f6878a790eefe6748f9990505fa788b0e5f8b47
“elisanuness” <elisanunessf.04@gmail.com>
2025-09-14 12:17:54 -0300

    add: Welcome component

--- FrontEnd/FrontEnd-Angular/src/app/app.component.ts
+++ FrontEnd/FrontEnd-Angular/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent],
+    HeaderComponent,
+    WelcomeComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
